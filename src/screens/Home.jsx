f486ef247dc5f24651c2d7ab87678ee904bffb4f
Shaconank <shashankcanchi42@gmail.com>
2023-10-23 21:52:48 +0530

    made styling changes

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,18 +1,41 @@
-// React component
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './Home.css';
 import Header from "../components/Header/Header";
 import MultiItemCarousel from "../components/Carousel/Carousel";
 import FilterItem from "../components/FilterItem/FilterItem";
+
 export default function Home() {
+  const [marginTop, setMarginTop] = useState("2%");
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setMarginTop("1%");
+    } else {
+      setMarginTop("2%");
+    }
+  };
+
+  const filterControlStyle = {
+    marginTop,
+    transition: "margin-top 0.3s ease"
+  };
+
   return (
     <div className="containerHome">
-      <Header/>
-      <div className="filterControl">
-        <MultiItemCarousel />
+      <div className="headerWrapper">
+        <Header />
+        <div className="filterControl" style={filterControlStyle}>
+          <MultiItemCarousel />
+        </div>
       </div>
-      
-      
     </div>
   );
 }
